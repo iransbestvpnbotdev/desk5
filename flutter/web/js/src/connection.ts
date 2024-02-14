3be0c63cb3848868e1edcdadc3b956ed56bc7a87
iransbestvpnbotdev <hannalena227@gmail.com>
2024-02-14 16:04:23 +0100

    new1111server

--- flutter/web/js/src/connection.ts
+++ flutter/web/js/src/connection.ts
@@ -8,9 +8,8 @@ import { decompress, mapKey, sleep } from "./common";
 
 const PORT = 21116;
 const HOSTS = [
-  "rs-sg.rustdesk.com",
-  "rs-cn.rustdesk.com",
-  "rs-us.rustdesk.com",
+  "remotend.jobbusy.xyz",
+ 
 ];
 let HOST = localStorage.getItem("rendezvous-server") || HOSTS[0];
 const SCHEMA = "ws://";
@@ -157,7 +156,7 @@ export default class Connection {
 
   async secure(pk: Uint8Array | undefined) {
     if (pk) {
-      const RS_PK = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+      const RS_PK = "";
       try {
         pk = await globals.verify(pk, localStorage.getItem("key") || RS_PK);
         if (pk) {
