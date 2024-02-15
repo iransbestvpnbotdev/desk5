fa62dc064b55f9eb9c35ace0cdc9c6a62c47744e
iransbestvpnbotdev <hannalena227@gmail.com>
2024-02-15 04:06:04 +0100

    new

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -91,7 +91,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["remotend.jobbusy.xyz"];
-pub const PUBLIC_RS_PUB_KEY: &str = "Ak6QCDI/gyH2AQsKL2QI8bKzanCAhNfOuorgo6EfPZY=";
+pub const PUBLIC_RS_PUB_KEY: &str = "19Wlg6LLFg2DksiPYtBsSsRylymO56Bce4AEzXFz998=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
